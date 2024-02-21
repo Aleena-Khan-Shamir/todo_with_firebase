c7b8ee929d6e04fc52ec558bce90dfde0da4be4c
usbooth <112530105+Aleena-Khan-Shamir@users.noreply.github.com>
2024-02-21 15:07:58 +0500

    todo .

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 import 'package:todo_with_firebase/config/config.dart';
-import 'package:todo_with_firebase/service/notiifcation/firebase_notification.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:timezone/data/latest.dart' as tz;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  NotificationServices().init();
   tz.initializeTimeZones();
   runApp(const MyApp());
   await Firebase.initializeApp(
